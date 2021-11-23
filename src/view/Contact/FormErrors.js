81c81647778d35e56a1c672ce7ee1421536508d5
Mariusz Janowski <mariusz.janowski@yahoo.pl>
2021-11-23 12:47:41 +0100

    messenger-app

--- src/view/Contact/FormErrors.js
+++ src/view/Contact/FormErrors.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./FormErrors.module.scss";
-
 export const FormErrors = ({ formErrors }) => (
+
   <div className={styles.formErrors}>
     <div className={styles.textErrors}>
       {formErrors && <p className={styles.textErrors}>{formErrors}</p>}
