935ea4264f73661adf9d18a5e00ee3624840cd37
Mariusz Janowski <mariusz.janowski@yahoo.pl>
2021-12-28 09:38:45 +0100

    author introduce

--- src/view/Head/Head.js
+++ src/view/Head/Head.js
@@ -1,28 +1,13 @@
-import React, { Suspense } from "react";
+import React from "react";
 import styles from "./Head.module.scss";
 import Flag from "../Navbar/Flag/Flag";
-import LoadingScreen from "react-loading-screen";
-import Logo from "../../components/BombaLogo/bomba_small.png";
 
 import BombaLogo from "../../components/BombaLogo/BombaLogo";
 import ToggleButton from "../../components/ToggleButton/ToggleButton";
 function Head({ theme, toggleTheme }) {
   return (
     <div id="home" className={styles.container}>
-      <Suspense
-        fallback={
-          <LoadingScreen
-            loading={true}
-            bgColor="#f1f1f1"
-            spinnerColor="#9ee5f8"
-            textColor="#676767"
-            logoSrc={Logo}
-            children="bombadesign.pl"
-          />
-        }
-      >
         <BombaLogo />
-      </Suspense>
       <ToggleButton theme={theme} toggleTheme={toggleTheme} />
       <div className={styles.flag}>
         <Flag />
