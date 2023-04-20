83ed7e81251fb3a537ec1b6b04030808915a07f1
BombaDesign <mariusz.janowski@yahoo.pl>
2023-04-20 19:27:30 +0200

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    "react/prop-types": "off",
-    "no-unused-vars": "off",
-  },
-};
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
